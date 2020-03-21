c2f4847fbb6d7b7c037ea2c1279f83d37c5db045
Gaurav <allmanpride@gmail.com>
2020-03-21 13:28:21 +0545

    before using pointer to pointer

--- emulator.c
+++ emulator.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+/* #include <limits.h> */
 #include <string.h>
 
 #define NOT_GATE 1
@@ -9,7 +10,30 @@
 
 /* enum {NOT,AND,OR} GateType; */
 
-char* gateName(int gcode){
+typedef struct {
+  int gateType;
+  int rank;
+  int inputs[2];
+  int output;
+} Gate;
+
+typedef struct {
+  int rank;
+  int number;
+  Gate* gates;
+} GateStack;
+
+typedef struct {
+  int inputs;
+  int outputs;
+  int nodes;
+  int gates;
+  int levels;
+  GateStack* stack;
+} Circuit;
+
+
+char* gateTypeName(int gcode){
   switch (gcode){
   case NOT_GATE:
     return "NOT";
@@ -22,32 +46,23 @@ char* gateName(int gcode){
   }
 }
 
-typedef struct {
-  int gateType;
-  int inputs[2];
-  int output;
-} Gate;
-
-typedef struct {
-  int rank;
-  int number;
-  Gate * gates;
-} GateStack;
-
-typedef struct {
-  int inputs;
-  int outputs;
-  int levels;
-  GateStack * stack;
-} Circuit;
-
+void addGateInStack(GateStack* gs, Gate* g){
+  int i;
+  for (i=0;i<gs->number;i++){
+    if (g==(gs->gates + i)){
+      return ;
+    }
+  }
+  gs->number += 1;
+  gs->gates = realloc(gs->gates,(gs->number)*sizeof(Gate));
+  *(gs->gates + gs->number-1) = *g;
+}
 
 void printGate(Gate g){
   printf("Gate Type: 1\n");
   printf("\tInput nodes:%d,%d\n",g.inputs[0],g.inputs[1]);
   printf("\tOutput node:%d",g.output);
 }
-
 
 int processGate(Gate g, int* inputList){
   switch(g.gateType) {
@@ -99,12 +114,73 @@ Gate parseGateDef(char* str){
   return g;
 }
 
-int processStack(Circuit* cir, char* infile, char* outfile){
+int processFile(Circuit* cir, char* infile, char* outfile){
   
+  while(1){
+    /*TODO*/
+  }
 }
 
+int max_nd(int a, int b){
+  if(a==-1 || b==-1){
+    return -1;
+  }
+  if(a>b){
+    return a;
+  }else{
+    return b;
+  }
+}
 
+void processGatesRank(int numGates, int numInput, int numNodes, Gate* gates){
+  int* nodesRank;
+  Gate* g;
+  int i,j,n1,n2,rank;
+  nodesRank = malloc(numNodes * sizeof(int));
+  for(i=0;i<numInput;i++){
+    *(nodesRank+i) = 0;
+  }
+  for(;i<numNodes;i++){
+    *(nodesRank+i) = -1;
+  }
 
+  for(j=0;j<numGates;j++){
+    for(i=0;i<numGates;i+=){
+      g = (gates+i);
+      n1 = *(g->inputs);
+      n2 = *(g->inputs+1);
+      rank = max_nd(*(nodesRank+n1),*(nodesRank+n2));
+      *(nodesRank+ g->output) = rank;
+      if (rank >-1){
+	g->rank = rank;
+      }
+    }
+  }
+}
+
+void processStack(Circuit* cir,int* nodesValue){
+  int i,j;
+  for(i=0; i < (cir->levels); i++){
+    for(j=0; j< (cir->stack->number);j++){
+      processGate(*(cir->stack->gates+j), nodesValue);
+    }
+  }
+}
+
+Circuit* makeCircuit(int gatesNumber, Gate* gates){
+  Circuit c;
+  int i,j,n;
+  n=0;
+  for(i=0;i<gatesNumber;i++){
+    n = max_nd(n,(gates+i)->rank);
+  }
+  if (n==-1){
+    printf("well well well,\n\tSee who made an error\n");
+  }
+  
+  
+  return &c;
+}
   
 int main(int argc,char* argv[]){
   int *inputList;
